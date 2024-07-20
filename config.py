3971b8f3fe1d19d7cbc108230413be1d077a1672
jaadisini <162589971+jaadisini@users.noreply.github.com>
2024-07-20 22:37:45 +0800

    Update config.py

--- config.py
+++ config.py
@@ -5,11 +5,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
+API_ID = int(getenv("API_ID", "23770828"))
+API_HASH = getenv("API_HASH", "2d3e87f244740e5c8286591940e24cd4")
 
-BOT_TOKEN = getenv("BOT_TOKEN")
-MONGO_DB_URI = getenv("MONGO_DB_URI", None)
+BOT_TOKEN = getenv("BOT_TOKEN", "6920452146:AAHXRoP_28ZxBlV-GkZ2tv_Gpd2ccdjR2QI")
+MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://sorybang:1234@cluster0.ww8a9ut.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
-OWNER_ID = int(getenv("OWNER_ID", 1318826936))
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/seriousvs_version10")
+OWNER_ID = int(getenv("OWNER_ID", "7149602071"))
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/speakupable")
